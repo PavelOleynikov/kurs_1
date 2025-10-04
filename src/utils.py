81f260d0e3c560cca38baae7d5d4b0877153b777
Pavel Oleynikov <Pavlushka19805@gmail.com>
2025-10-05 01:06:41 +0400

    add logging in utils

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,21 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 from pandas import DataFrame
+import logging
 
 load_dotenv()  # загрузка переменных из .env-файла
+
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
+log_file = os.path.join(log_dir, "utils.log")
+
+logger = logging.getLogger("utils")  # создаем логер с именем модуля
+logger.setLevel(logging.DEBUG)  # устанавливаем уровень логирования
+file_handler = logging.FileHandler(log_file, mode="w", encoding="utf-8")  # путь записи логов
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)  # устанавливаем формат вывода логов
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def current_time_greeting() -> str:
@@ -17,12 +30,16 @@ def current_time_greeting() -> str:
     user_hour = user_datetime.hour  # актуальный час
 
     if 6 <= user_hour < 12:
+        logger.info("актуальное время суток")
         return "Доброе утро"
     elif 12 <= user_hour < 18:
+        logger.info("актуальное время суток")
         return "Добрый день"
     elif 18 <= user_hour < 23:
+        logger.info("актуальное время суток")
         return "Добрый вечер"
     else:
+        logger.info("актуальное время суток")
         return "Доброй ночи"
 
 
@@ -33,6 +50,7 @@ def get_slice_data(date_time: str) -> list[str]:
     start_month = dt.replace(day=1)  # получаем 1-е число месяца
 
     # возвращаем список дат периода ["01.01.2021 00:00:00", "15.01.2021 00:00:00"]
+    logger.info("выбранный период дат для фильтрации")
     return [start_month.strftime("%d.%m.%Y %H:%M:%S"), dt.strftime("%d.%m.%Y %H:%M:%S")]
 
 
@@ -50,6 +68,7 @@ def get_cut_from_excel(path_to_file: str, period_date: list[str]) -> DataFrame:
 
     # сортируем полученный срез по возрастанию даты
     sorted_df = filtered_df.sort_values(by="Дата операции", ascending=True)
+    logger.info("список операций за выбранный период")
     return sorted_df
 
 
@@ -69,6 +88,7 @@ def get_card_with_spent(sorted_df: DataFrame) -> list[dict]:
             row = {"last_digits": last_digits, "total_spent": total_spent, "cashback": cashback}
             operations_spent.append(row)
 
+    logger.info("список карт с расходами и кэшбэком")
     return operations_spent
 
 
@@ -89,6 +109,7 @@ def get_transactions_by_pay(sorted_df: DataFrame) -> list[dict]:
 
     top_5 = top_transactions[:5]
 
+    logger.info("ТОП-5 транзакций по сумме платежа")
     return top_5
 
 
@@ -98,6 +119,7 @@ def get_currency_rates(path_to_file: str) -> list[dict]:
     api_key = os.getenv("API_KEY")  # Получение значения переменной API_KEY из .env-файла
 
     if not api_key:
+        logger.error("API_KEY не найден")
         raise Exception("API_KEY не найден в переменных окружения")
 
     currency_rates = []
@@ -111,13 +133,14 @@ def get_currency_rates(path_to_file: str) -> list[dict]:
             response = requests.get(url, headers=headers)
 
             if response.status_code != 200:
+                logger.error(f"API error: {response.status_code}")
                 raise Exception(f"API error: {response.status_code}")
 
             response_currency = response.json()
             currency = response_currency["query"]["from"]
             rate = round(response_currency["result"], 2)
             currency_rates.append({"currency": currency, "rate": rate})
-
+    logger.info("текущий курс валют")
     return currency_rates
 
 
@@ -127,6 +150,7 @@ def get_stock_prices(path_to_file: str) -> list[dict]:
     api_key = os.getenv("API_KEY_TD")  # Получение значения переменной API_KEY из .env-файла
 
     if not api_key:
+        logger.error("API_KEY не найден")
         raise Exception("API_KEY_TD не найден в переменных окружения")
 
     stock_prices = []
@@ -138,11 +162,12 @@ def get_stock_prices(path_to_file: str) -> list[dict]:
             response = requests.get(url)
 
             if response.status_code != 200:
+                logger.error(f"API error: {response.status_code}")
                 raise Exception(f"API error: {response.status_code}")
 
             response_stocks = response.json()
             stock = response_stocks["meta"]["symbol"]
             price = round((float(response_stocks["values"][0]["open"])), 2)
             stock_prices.append({"stock": stock, "price": price})
-
+    logger.info("текущая стоимость акций")
     return stock_prices
