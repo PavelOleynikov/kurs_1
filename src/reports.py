45f9cf5a945be2658c9e2cf7b31c8ede6a895ade
Pavel Oleynikov <Pavlushka19805@gmail.com>
2025-10-08 15:16:41 +0400

    add func in  reports and update project

---
+++ src/reports.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+import pandas as pd
+from datetime import datetime
+import logging
+import os
+
+from dateutil.relativedelta import relativedelta
+
+
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
+log_file = os.path.join(log_dir, "reports.log")
+
+logger = logging.getLogger("reports")  # создаем логер с именем модуля
+logger.setLevel(logging.DEBUG)  # устанавливаем уровень логирования
+file_handler = logging.FileHandler(log_file, mode="w", encoding="utf-8")  # путь записи логов
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)  # устанавливаем формат вывода логов
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+    """
+    Функция возвращает траты по заданной категории за последние три месяца (от переданной даты).
+    """
+
+    if date is None:
+        dt = datetime.now()  # актуальное время
+    else:
+        dt = datetime.strptime(date, "%d.%m.%Y")  # задаем входной формат
+    start_month = dt - relativedelta(months=3)  # получаем дату начала трехмесячного периода
+
+    # возвращаем список дат периода ["15.01.2020", "15.04.2020"]
+    logger.info("задан период дат для фильтрации")
+    time_period = [start_month.strftime("%d.%m.%Y"), dt.strftime("%d.%m.%Y")]
+
+    transactions["Дата операции"] = pd.to_datetime(
+        transactions["Дата операции"], dayfirst=True
+    )  # перевод строки в объект pd
+    start_date = datetime.strptime(time_period[0], "%d.%m.%Y")
+    end_date = datetime.strptime(time_period[1], "%d.%m.%Y")
+
+    # фильтруем и получаем срез по интервалу дат
+    logger.info("получен срез операций по интервалу дат")
+    filtered_df = transactions[
+        (transactions["Дата операции"] >= start_date) & (transactions["Дата операции"] <= end_date)
+    ]
+
+    list_dict = filtered_df.to_dict(orient="records")  # преобразуем df в список словарей
+    result_by_category = []
+    total_spend = 0
+    for operation in list_dict:
+        if operation["Категория"] == category:
+            result_by_category.append(operation)
+    for transaction in result_by_category:
+        total_spend += transaction["Сумма операции с округлением"]
+    result_pd = pd.DataFrame(result_by_category)  # преобразуем в DataFrame
+    print(f"Траты по категории '{category}' {round(total_spend, 2)}")
+    logger.info("получен список трат по категории")
+    return result_pd
--- src/services.py
+++ src/services.py
@@ -1,11 +1,7 @@
 import json
 import logging
 import os
-
-import pandas as pd
 import re
-from config import PATH_TO_EXCEL
-
 
 log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
 log_file = os.path.join(log_dir, "services.log")
@@ -18,10 +14,6 @@ file_formatter = logging.Formatter(
 )  # устанавливаем формат вывода логов
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
-
-logger.info(f"получение данных из файла {PATH_TO_EXCEL}")
-df = pd.read_excel(PATH_TO_EXCEL)  # читаем файл xlsx и получаем объект DataFrame
-list_dict = df.to_dict(orient="records")  # преобразуем df в список словарей
 
 
 def service_search(operations_list: list[dict], search: str) -> json:
@@ -39,7 +31,3 @@ def service_search(operations_list: list[dict], search: str) -> json:
     json_result = json.dumps(result, ensure_ascii=False, indent=4, sort_keys=True)
     logger.info("получен список операций по строке поиска")
     return json_result
-
-
-if __name__ == "__main__":
-    print(service_search(list_dict, "жкх"))
